e69a256e7f2965a4910af0a14c2caa0a19688733
Titus Kiplagat <tituskiplagat210@gmail.com>
2022-12-31 11:28:23 +0300

    Pointer to array

--- Pointers/ARRAY/pointerToArray.c
+++ Pointers/ARRAY/pointerToArray.c
@@ -5,9 +5,10 @@ int main(){
     int arr[5] = {1,2,3,4};
     int i = 0;
 
+
     // declare and initialize pointer to array
     int *ptr = arr;
-
+    int (*ptr_2)[5] = &arr;
 
     // loop and print items in array
     for( i ; i <= sizeof(arr); i++){
